82ee87921ad6f8f7f3372a49bb8c46acce4a7d6c
[prasanna] <prasanna>
2022-05-24 14:55:56 +0530

    [prasanna]: checking contacts

---
+++ addressbook/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using addressbook;
+internal class program
+{
+    public static List<contacts> person = new List<contacts>();
+
+    public static void main(string[] args)
+    {
+        addressbook.person.creatcontact();
+        addressbook.person.displaycontacts();
+        addressbook.person.editcontacts();
+    }
+}
---
+++ addressbook/contacts.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook
+{
+    internal class contacts
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int zip { get; set; }
+        public string phoneNo { get; set; }
+        public string email { get; set; }
+
+    }
+}
